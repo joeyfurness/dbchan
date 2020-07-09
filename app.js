c9e2cca51190028583638a6f747046ef3dc5d483
josephmwells <joester3@gmail.com>
2020-07-09 22:34:45 +0000

    Added 481 route

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello, I am your Express App!')
+	res.send('Hello, I am your Express App! This is some text!')
+});
+
+app.get('/418', (req, res) => {
+	res.set('X-Custom-Header', 'true');
+	res.status(418);
+	res.send('Error: 418');
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
