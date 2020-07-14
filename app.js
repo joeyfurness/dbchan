1ce8b785131ee6a84e372aba7cb5cecd9dfbfccb
josephmwells <joester3@gmail.com>
2020-07-14 19:32:30 +0000

    Configured dotfiles and server automation

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+import 'dotenv/config'
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+
+const app = express()
+const port = process.env.PORT
+app.disable('x-powered-by')
+
+app.use(cors()) // Fixes Cross Origin Resource Sharing
+app.use(json()) // Encodes request as json
+app.use(urlencoded({ extended: true})) // Idk exactly what this does but it fixes some stuff in the header
+app.use(morgan('dev')) // Logs HTTP to the console
 
 app.get('/', (req, res) => {
-	res.send('Hello, I am your Express App! This is some text!')
-});
+	res.send('Hello, I am your Express App!')
+})
 
 app.get('/418', (req, res) => {
-	res.set('X-Custom-Header', 'true');
-	res.status(418);
-	res.send('Error: 418');
-});
+	res.set('X-Custom-Header', 'true')
+	res.status(418)
+	res.send('Error: 418')
+})
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+app.listen(port, () => console.log(`App listening on port ${port}`))
