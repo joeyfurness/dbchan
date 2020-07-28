9f1ffee0efd2d97238ed926c2812ced1e8d95145
josephmwells <joester3@gmail.com>
2020-07-28 13:25:39 -0700

    Added Front-end of application

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="dbchan logo" src="./assets/db_logo.png">
+    <h1>Welcome to dbchan, we'll be up and running soon.</h1>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #333333;
+  margin-top: 60px;
+}
+</style>
