fcadc19a2fd716a045d78f31e683f2850890df6b
josephmwells <joester3@gmail.com>
2020-08-08 16:38:45 -0700

    Update thread service

---
+++ server/components/thread/service.js
@@ -0,0 +1,16 @@
+const Thread = require('./model');
+const messageServices = require('../message/service');
+
+const getThreadById = (id) => {
+  return Thread.findById(id).exec();
+}
+
+const createMessageAndAddToThread = async (data) => {
+  const message = await messageServices.createMessage(data);
+  return Thread.findByIdAndUpdate(data.threadId, { $push: {messages: message}}, {new: true}).exec();
+}
+
+module.exports = {
+  getThreadById,
+  createMessageAndAddToThread,
+};
